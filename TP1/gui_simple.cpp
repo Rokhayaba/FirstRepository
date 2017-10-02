3615881abdd1bbb1683cea3307dc7e9f89968cc1
Rokhaya Ba <iya837@hotmail.com>
2017-10-02 14:52:35 +0200

    jout de TP1 dans mon nouveau depot

---
+++ TP1/gui_simple.cpp
@@ -0,0 +1,24 @@
+#include <QApplication>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+
+  QPushButton *quitButton = new QPushButton("&Quit");
+  QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+  QVBoxLayout *layout = new QVBoxLayout;
+  layout->addWidget(quitButton);
+
+  QWidget window;
+  window.setLayout(layout);
+  window.setGeometry(100, 100, 500, 400);
+  window.show();
+
+  return a.exec();
+}
+// /usr/include/qt4/QtGui/QApplication
+// /usr/include/qt4/QtGui/QPushButton
+// /usr/include/qt4/QtGui/QVBoxLayout
